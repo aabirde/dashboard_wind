659ad7e74ab5979fff564e1550ff9d333b816608
Aabir De <aabiranishde@gmail.com>
2025-08-06 17:29:41 +0530

    commit 13

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import ReportsComponent from './components/ReportsComponent';
 import DashboardPage from './pages/DashboardPage';
 import WelcomePage from './pages/WelcomePage';
